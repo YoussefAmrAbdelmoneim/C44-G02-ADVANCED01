a964ab2c894874c712f19e3468b0046ea56f5e94
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-08-25 19:49:37 +0300

    Q3

--- Assignment 1 Advanced/Program.cs
+++ Assignment 1 Advanced/Program.cs
@@ -13,6 +13,20 @@ namespace Assignment_1_Advanced
         //    }
         //}
         #endregion
+        #region Q3
+        //public static List<int> EvenNumbersList(List<int> list)
+        //{
+        //    List<int> evenList = new List<int>();
+        //    for (int i = 0; i < list.Count; i++)
+        //    {
+        //        if (list[i] % 2 == 0)
+        //        {
+        //            evenList.Add(list[i]);
+        //        }
+        //    }
+        //    return evenList;
+        //}
+        #endregion
         static void Main(string[] args)
         {
             #region Q1
@@ -25,6 +39,14 @@ namespace Assignment_1_Advanced
             #region Q2
             //ArrayList arrayList = new ArrayList() { 1,2,3,4,5};
             //ReversedArrayList(arrayList);
+            #endregion
+            #region Q3
+            //List<int> list = new List<int>() {0,1,2,3,4,5,6};
+            //List<int> evenList= EvenNumbersList(list);
+            //foreach (int item in evenList)
+            //{
+            //    Console.WriteLine(item);
+            //}
             #endregion
         }
         }
